bdc49adcb3a1d853007cb1ca010e01a8308e0a8d
jimStev2447 <jim.stevenson2447@gmail.com>
2021-12-04 18:26:09 +0000

    day 3 finished

--- day3/day3.js
+++ day3/day3.js
@@ -1,20 +1,16 @@
 exports.findGamma = (binaries) => {
   let bin = "";
-  const bitLength = binaries[0].length;
-  for (let i = 0; i < bitLength; i++) {
-    const mostCommon = mostCommonBinAtIndex(binaries, i);
+  findMostCommonAtEachIndex(binaries, (mostCommon) => {
     bin += mostCommon;
-  }
+  });
   return bin;
 };
 
 exports.findEpsilon = (binaries) => {
   let bin = "";
-  const bitLength = binaries[0].length;
-  for (let i = 0; i < bitLength; i++) {
-    const mostCommon = mostCommonBinAtIndex(binaries, i);
+  findMostCommonAtEachIndex(binaries, (mostCommon) => {
     bin += mostCommon === "0" ? "1" : "0";
-  }
+  });
   return bin;
 };
 
@@ -28,5 +24,41 @@ exports.findPowerConsumption = (data) => {
 function mostCommonBinAtIndex(binaries, i) {
   let count = 0;
   binaries.forEach((bin) => (count += +bin[i]));
-  return count > binaries.length / 2 ? "1" : "0";
+  return count >= binaries.length / 2 ? "1" : "0";
 }
+
+exports.oxygenGenRating = (binaries) => {
+  for (let i = 0; i < binaries[0].length; i++) {
+    const mostCommon = mostCommonBinAtIndex(binaries, i);
+    if (binaries.length > 1) {
+      binaries = binaries.filter((bin) => bin[i] === mostCommon);
+    }
+  }
+  return binaries[0];
+};
+
+exports.c02ScrubberRating = (binaries) => {
+  for (let i = 0; i < binaries[0].length; i++) {
+    const mostCommon = mostCommonBinAtIndex(binaries, i);
+    const leastCommon = mostCommon === "1" ? "0" : "1";
+    const newBinaries = binaries.filter((bin) => bin[i] === leastCommon);
+    if (newBinaries.length === 0) return binaries[0];
+    if (newBinaries.length === 1) return newBinaries[0];
+    binaries = newBinaries;
+  }
+};
+
+function findMostCommonAtEachIndex(binaries, cb) {
+  const bitLength = binaries[0].length;
+  for (let i = 0; i < bitLength; i++) {
+    const mostCommon = mostCommonBinAtIndex(binaries, i);
+    cb(mostCommon);
+  }
+}
+
+exports.findLifeSupportRating = (data) => {
+  const bins = data.split("\n");
+  const o2 = this.oxygenGenRating(bins);
+  const co2 = this.c02ScrubberRating(bins);
+  return parseInt(o2, 2) * parseInt(co2, 2);
+};
--- day3/day3.test.js
+++ day3/day3.test.js
@@ -1,4 +1,11 @@
-const { findGamma, findEpsilon, findPowerConsumption } = require("./day3.js");
+const {
+  findGamma,
+  findEpsilon,
+  findPowerConsumption,
+  findLifeSupportRating,
+  oxygenGenRating,
+  c02ScrubberRating,
+} = require("./day3.js");
 const { readFile } = require("fs");
 
 describe("findGamma", () => {
@@ -43,6 +50,77 @@ describe("Advent pt1", () => {
   test("pt1", (done) => {
     readFile("./day3/day3.txt", "utf-8", (err, data) => {
       expect(findPowerConsumption(data)).toBe(3959450);
+      done();
+    });
+  });
+});
+
+describe("oxygenGenRating", () => {
+  test("given a single bit will return that bit", () => {
+    expect(oxygenGenRating(["1"])).toBe("1");
+  });
+  test("given 2 bits will return the most common and if tied 1", () => {
+    expect(oxygenGenRating(["0", "0"])).toBe("0");
+    expect(oxygenGenRating(["0", "1"])).toBe("1");
+    expect(oxygenGenRating(["1", "1"])).toBe("1");
+  });
+  test("given larger bit numbers, will return the one with the most common number at each index", () => {
+    expect(oxygenGenRating(["010", "101", "110"])).toBe("110");
+  });
+  test("acceptance test", () => {
+    const bins = [
+      "00100",
+      "11110",
+      "10110",
+      "10111",
+      "10101",
+      "01111",
+      "00111",
+      "11100",
+      "10000",
+      "11001",
+      "00010",
+      "01010",
+    ];
+    expect(oxygenGenRating(bins)).toBe("10111");
+  });
+});
+
+describe("c02ScrubberRating", () => {
+  test("given a single bit will return that bit", () => {
+    expect(c02ScrubberRating(["1"])).toBe("1");
+  });
+  test("given 2 bits will return the least common and if tied 0", () => {
+    expect(c02ScrubberRating(["0", "0"])).toBe("0");
+    expect(c02ScrubberRating(["0", "1"])).toBe("0");
+    expect(c02ScrubberRating(["1", "1"])).toBe("1");
+  });
+  test("given larger bit numbers, will return the one with the most common number at each index", () => {
+    expect(c02ScrubberRating(["010", "101", "110"])).toBe("010");
+  });
+  test("acceptance test", () => {
+    const bins = [
+      "00100",
+      "11110",
+      "10110",
+      "10111",
+      "10101",
+      "01111",
+      "00111",
+      "11100",
+      "10000",
+      "11001",
+      "00010",
+      "01010",
+    ];
+    expect(c02ScrubberRating(bins)).toBe("01010");
+  });
+});
+
+describe("Advent pt2", () => {
+  test("pt2", (done) => {
+    readFile("./day3/day3.txt", "utf-8", (err, data) => {
+      expect(findLifeSupportRating(data)).toBe(7440311);
       done();
     });
   });
